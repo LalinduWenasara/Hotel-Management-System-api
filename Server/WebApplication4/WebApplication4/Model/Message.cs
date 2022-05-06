09403d176e8ca98b48100b3079ca31e416c6e028
LalinduWenasara <lalinduwenu@gmail.com>
2022-05-07 01:51:39 +0530

    update

---
+++ Server/WebApplication4/WebApplication4/Model/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication4.Model
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string MessageBody { get; set; }
+        public string DateTime { get; set; }
+        public string RoomID { get; set; }
+        public string SenderID { get; set; }
+    }
+}
--- Server/WebApplication4/WebApplication4/Model/User.cs
+++ Server/WebApplication4/WebApplication4/Model/User.cs
@@ -3,6 +3,12 @@
     public class User
     {
         public int UserId { get; set; }
-        public string UserName { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserNIC { get; set; }
+        public string UserMobile { get; set; }
+
+
     }
 }

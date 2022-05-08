ca9d32b1215f0c787dc2f24d88638bb16b1427d0
Hiranya Ravinath Soysa <67453323+HiranyaRavinath@users.noreply.github.com>
2022-05-08 21:39:14 +0530

    viewjobs

---
+++ WebClient/viewjobs.php
@@ -0,0 +1,125 @@
+<?php
+    $fname = "lalindu";
+    $lname = "wenasara";
+    $contact ="0000007777";
+    $email="lalindu@gmail.com";
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign Up Form by Colorlib</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="css/dashboard.css">
+</head>
+<body>
+
+    <div class="main">
+
+        <!-- Sign up form -->
+        <section class="signup">
+            <div class="container">
+                <div class="signup-content">
+                    <div class="signup-form">
+                        <h2 class="form-title">View Jobs</h2>
+
+
+                        <div class="col-md-12">
+                                <div class="card shadow mb-4">
+                                    <div class="card-header py-3">
+                                        <h6 class="m-0 font-weight-bold text-primary">Jobs</h6>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="table-responsive">
+                                            <table class="table">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Job No</th>
+                                                        <th>Room No</th>
+                                                        <th>Contact</th>
+                                                        <th>Customer Name</th>
+                                                        <th></th>
+                                                        
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    
+                                                        <tr>
+                                                            <td>Hiranya</td>
+                                                            <th>055</th>
+                                                            <td>0221526</td>
+                                                            <td>Hiranya</td>
+                                                    
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Lalindu</td>
+                                                            <th>025055</th>
+                                                            <td>0221526</td>
+                                                            <td>Lalindu</td>
+                                                    
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Hiranya</td>
+                                                            <th>055</th>
+                                                            <td>0221526</td>
+                                                            <td>Hiranya</td>
+                                                    
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Lalindu</td>
+                                                            <th>025055</th>
+                                                            <td>0221526</td>
+                                                            <td>Lalindu</td>
+                                                    
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Hiranya</td>
+                                                            <th>055</th>
+                                                            <td>0221526</td>
+                                                            <td>Hiranya</td>
+                                                    
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Lalindu</td>
+                                                            <th>025055</th>
+                                                            <td>0221526</td>
+                                                            <td>Lalindu</td>
+                                                    
+                                                        </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                            
+
+                         
+
+
+                                          
+
+
+
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
+
+    <!-- JS -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="js/sign.js"></script>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</html>
